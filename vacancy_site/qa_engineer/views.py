7b9179f7272e97cfeb0ca30a3bb145842a706446
kav0ish0 <vovaozornin18@gmail.com>
2023-01-14 20:14:21 +0500

    upload files

---
+++ vacancy_site/qa_engineer/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.template import Template
+from vacancies.vacancies import get_yesterday_vacancies
+from .models import Post
+
+
+def get_content(name: str):
+    content = \
+        {'content': Post.objects.filter(title=name).first(),
+         'title': name}
+    return content
+
+
+def home(request):
+    content = get_content('Главная')
+    return render(request, 'qa_engineer/index.html', content)
+
+
+def geography(request):
+    content = get_content('География')
+    return render(request, 'qa_engineer/index.html', content)
+
+
+def demand(request):
+    content = get_content('Востребованность')
+    return render(request, 'qa_engineer/index.html', content)
+
+
+def recent_vacancies(request):
+    vacancies = get_yesterday_vacancies()
+    context = {
+        'vacancies': vacancies
+    }
+    return render(request, "qa_engineer/recent-vacancies.html", context)
+
+
+def skills(request):
+    content = get_content('Навыки')
+    return render(request, 'qa_engineer/index.html', content)
